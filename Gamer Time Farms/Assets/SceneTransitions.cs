741686b01cacaa50962aad828b8d7d660ddabc44
Luke Hellebusch <lukehellebusch@gmail.com>
2021-04-10 16:30:38 -0500

    Main Menu
    main menu complete with animation and buttons

---
+++ Gamer Time Farms/Assets/SceneTransitions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public void toMenu(){
+      SceneManager.LoadScene(0);
+    }
+
+    public void quitGame(){
+      Application.Quit();
+    }
+}
